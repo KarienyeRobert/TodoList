a3f69e6a323caaac72e9747a87bfdffde7a9b1c4
KarienyeRobert <karienyerobert@gmail.com>
2024-01-28 21:39:45 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import Task  from './components/task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    {/* {Today's tasks} */}
+    <View style={styles.taskWrapper}>
+      <Text style={styles.sectionTitle}>Today's Tasks</Text>
+      <View style={styles.items}>
+        {/* {this is where tasks will go!} */}
+        <Task text={'Task 1'}/>
+        <Task text={'Task 2'}/>
+      </View>
+
+    </View>
+
+    {/* {write a task} */}
+     <KeyboardAvoidingView
+     behavior = {Platform.OS==='ios'? 'padding':'height'}
+     style={styles.writeTaskWrapper}>
+      <TextInput style={styles.input} placeholder ={'Write a Task!!'}/>
+
+      <TouchableOpacity>
+        <View style= {styles.addWrapper}>
+          <Text style= {styles.addText}>+</Text>
+        </View>
+      </TouchableOpacity>
+     </KeyboardAvoidingView>
+
     </View>
   );
 }
@@ -13,8 +36,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e8eaed',
   },
+  taskWrapper: {
+    paddingTop:80,
+    paddingHorizontal:20,
+  },
+  sectionTitle: {
+    fontSize:24,
+    fontWeight: 'bold'
+
+  },
+  items: {
+    marginTop:30,
+  },
+  writeTaskWrapper:{},
+  input:{},
 });
